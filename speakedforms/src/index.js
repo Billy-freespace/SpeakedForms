b360b07d2bab314a5f3b3a22b6f751f4ec325173
Gladys Alesandra Yagi Vásquez <ale.yagi.v@gmail.com>
2022-12-23 00:03:10 -0500

    feat: Folder Estructure
    Estructura de las carpetas del proyecto

--- speakedforms/src/index.js
+++ speakedforms/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './pages/home/Home';
+import './estilos/css/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Home/>
   </React.StrictMode>
 );
 
